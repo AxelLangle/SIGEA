b4d4c35ead7742c8d281be99332b441c2d0e143c
AxelLangle <changguito.lc@gmail.com>
2025-06-19 15:34:02 -0600

    Se agrego el archivo app.py y la ruta principal se dirigio al login

---
+++ Backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify, render_template
+import re
+from queue import Queue
+import sqlite3
+import pypdf
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
