2951a34271d15a472f39c178f85151ac872b3a25
AxelLangle <changguito.lc@gmail.com>
2025-07-01 09:51:10 -0600

    Se ajusto la ruta del html registro y se agregaron estilos

---
+++ Database/sqlite3.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+
+conn = sqlite3.connect('usuarios.db')
+c = conn.cursor()
+c.execute('''
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        apellido_paterno TEXT NOT NULL,
+        apellido_materno TEXT NOT NULL,
+        correo TEXT NOT NULL UNIQUE,
+        contrasena TEXT NOT NULL
+    )
+''')
+conn.commit()
+conn.close()
+
+if __name__ == '__main__':
+    sqlite3.run(debug=True)
--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+
+import sqlite3
 from flask import Flask, request, jsonify, render_template
 import re
 from queue import Queue
 import sqlite3
-import pypdf
+
 
 app = Flask(__name__, template_folder='Backend/templates')
 
 @app.route('/')
 def login():
     return render_template('login.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
